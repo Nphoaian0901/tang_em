46d26da60c1d904e430ebe9594356aedfa9d8ba4
HoaiAn <nphoaian.nhh@gmail.com>
2022-06-05 20:54:23 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Tui có điều này muốn hỏi Bà, Bà nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Tui biết bà thích tui phải không phải không ? 😙',
+    desc: 'Bà không trả lời hoặc thoát ra, tức là Bà thích Tui đó nha :> ',
+    btnYes: 'Có, tui thật sự thích Ông 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Tui biết đi vì sao Bà thích Tui 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Ông đẹp trai đó Ahiiiii 😊😊',
+    mess: 'tui biết mà, Tui cũng thích bà lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tui  nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/PhungQuocDinh'
+}
